3e1a5c0476313cf1cff4ed3b4ec0c371037e2509
azam alias <azamalias@gmail.com>
2016-06-07 11:20:17 +0800

    initial commit

---
+++ NCronTabTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NCrontab;
+
+namespace NCronTabTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystring = "0 0 6 29 2 1";
+            var schedule = CrontabSchedule.Parse(mystring, new CrontabSchedule.ParseOptions { IncludingSeconds = true });
+            Console.WriteLine(schedule.GetNextOccurrence(DateTime.UtcNow));
+        }
+    }
+}
